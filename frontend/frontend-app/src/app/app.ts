3bb0762d8bae1c22162833e0cc4a5991eb128a75
Lucas Leitz <lucasleitz@outlook.com>
2026-04-28 23:45:44 -0400

    added frontend features like grid size, clearing board, saving image, and updated css to be like kid pix

--- frontend/frontend-app/src/app/app.ts
+++ frontend/frontend-app/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import html2canvas from 'html2canvas';
+
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,53 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.css'
 })
 export class App {
+  @ViewChild('board') boardElement!: ElementRef;
+
   selectedColor = '#d93a3a';
 
   rows = 20;
   cols = 20;
 
+  isPainting = false;
+
   cells = Array.from({ length: this.rows * this.cols }, () => '#ffffff');
 
   paintCell(index: number): void {
     this.cells[index] = this.selectedColor;
+  }
+
+  startPainting(index: number): void {
+    this.isPainting = true;
+    this.paintCell(index);
+  }
+
+  stopPainting() {
+    this.isPainting = false;
+  }
+
+  paintCellOnDrag(index: number): void {
+    if(this.isPainting) {
+      this.paintCell(index);
+    }
+  }
+
+  clearBoard(): void {
+    this.cells = Array.from({ length: this.rows * this.cols }, () => '#ffffff');
+  }
+
+  changeGridSize(size: number): void {
+    this.rows = size;
+    this.cols = size;
+    this.clearBoard();
+  }
+
+  saveBoardAsImage(): void {
+    html2canvas(this.boardElement.nativeElement).then(canvas => {
+      const link = document.createElement('a');
+      link.download = 'pixel-board.png';
+      link.href = canvas.toDataURL('image/png');
+      link.click();
+    });
   }
 
 }
