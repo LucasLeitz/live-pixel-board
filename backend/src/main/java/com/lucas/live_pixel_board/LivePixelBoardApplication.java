311b24b4a68a9cb018fa93f0d6ff6a51ddd8b58c
Lucas Leitz <lucasleitz@outlook.com>
2026-04-29 23:03:39 -0400

    Adding backend and health controller

---
+++ backend/src/main/java/com/lucas/live_pixel_board/LivePixelBoardApplication.java
@@ -0,0 +1,12 @@
+package com.lucasleitz.live_pixel_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivePixelBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivePixelBoardApplication.class, args);
+    }
+}
